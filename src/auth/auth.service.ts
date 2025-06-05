75c01751f7abcb3c48b6ca40c66d9bd56cc606e0
Think Dev <hocine.aachiq@gmail.com>
2025-06-05 11:11:44 +0100

    fix auth notes & start conatct module then back to auth with jwt ...

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,27 +17,36 @@ export class AuthService {
     return this.usersRepository.findOne({ where: { email } });
   }
 
-  private async create(
-    name: string,
-    email: string,
-    password: string,
-  ): Promise<User> {
+  private async create(userDto: SignUpRequestDto): Promise<User> {
+    const { name, email, password } = userDto;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = this.usersRepository.create({
+
+    // behind scene this method "create" instance new object from SignUpRequestDto class
+    // User user = new User();
+    // // dto gotten from params of method it's like "userDto"
+    // // since "userDto" is a class so before we pass object to method as params , we instance this
+    // // object frist from class userDto and we pass it to service "cretaeuSerService(user dtoUser)"
+    // user.setName(dto.getName())
+    // user.setEmail(dto.getEmail())
+    // user.setAge(dto.getAge())
+    //
+
+    const createdUser = this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
     });
-    return this.usersRepository.save(user);
+    return this.usersRepository.save(createdUser);
   }
 
   async signup(dto: SignUpRequestDto): Promise<SignUpResponseDto> {
-    const existingUser = await this.findByEmail(dto.email);
+    const { name, email, password } = dto;
+    const existingUser = await this.findByEmail(email);
     if (existingUser) {
       throw new ConflictException('Email already in use');
     }
 
-    const user = await this.create(dto.name, dto.email, dto.password);
+    const user = await this.create(dto);
     return new SignUpResponseDto(user);
   }
 }
