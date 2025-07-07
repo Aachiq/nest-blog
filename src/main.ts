e1c8ad40dc38de3fc6db07eb5370a3d9a8fae98e
Think Dev <hocine.aachiq@gmail.com>
2025-07-07 11:46:29 +0100

    add cors feature

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // add cors
+  app.enableCors({
+    origin: `http://localhost:3001`,
+    methods: ['POST', 'GET'],
+    credentials: true, // this used to enable use 'Authorization ...'
+  });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
